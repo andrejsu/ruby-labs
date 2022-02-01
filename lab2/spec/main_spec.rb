5d00312d726224d69d0801d3c4bb8fa57a90ae3f
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-02-01 03:33:31 +0300

    RL-9 Added tests for Pokemons methods

--- lab2/spec/main_spec.rb
+++ lab2/spec/main_spec.rb
@@ -17,3 +17,36 @@ RSpec.describe '#foo' do
     expect(foo(word)).to eq(word.reverse)
   end
 end
+
+RSpec.describe '#add_pokemons' do
+  it 'returns an array with Pokemons' do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('Pikachu', 'Yellow',
+                                                               'Snorlax', 'Light Blue',
+                                                               'Charizard', 'Orange')
+    expect(add_pokemons(3)).to eq([{ 'Pikachu' => 'Yellow' },
+                                   { 'Snorlax' => 'Light Blue' },
+                                   { 'Charizard' => 'Orange' }])
+  end
+end
+
+RSpec.describe '#validate_int_input' do
+  it 'returns an int' do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('3')
+    expect(validate_int_input).to eq(3)
+  end
+
+  it 'returns an error message' do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('', '5!', 0)
+    expect(validate_int_input).to eq(0)
+  end
+end
+
+RSpec.describe '#format_pokemons' do
+  it 'returns empty array' do
+    expect(format_pokemons([])).to eq('[]')
+  end
+
+  it 'returns array with one pokemon' do
+    expect(format_pokemons([{ 'Pikachu' => 'Yellow' }])).to eq('[{ name: \'Pikachu\', color: \'Yellow\' }]')
+  end
+end
