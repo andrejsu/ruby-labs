d5b1712c66f02475d9cb20e92a62e475d3e6cd78
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-02-21 01:33:15 +0300

    RL-18 Added the basic functionality of the application

--- lab6/app/models/user.rb
+++ lab6/app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 100 }
   validates :email, presence: true, length: { maximum: 150 },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def format_user
+    "#{first_name} #{last_name} [#{email}]"
+  end
 end
