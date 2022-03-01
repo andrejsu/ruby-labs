197689cc36b2c59755b81baf687818002413f9dd
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-03-01 17:25:03 +0300

    RL-19 Integrated authentication into the project from RL-18

--- lab7/app/models/user.rb
+++ lab7/app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reports
+
+  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :last_name, presence: true, length: { maximum: 100 }
+
+  def format_user
+    "#{first_name} #{last_name} [#{email}]"
+  end
 end
